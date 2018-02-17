b7ea42f1e97457b90161c9ca2daa666d50c224ce
João Felipe <jo_5919@hotmail.com>
2018-02-17 10:53:37 -0200

    Building the services

--- loader.js
+++ loader.js
@@ -1,11 +1,3 @@
-const port = 3003;
-const bodyParser = require('body-parser');
-const express = require('express');
-const server = express();
-
-server.use(bodyParser.urlencoded({extended: true}));
-server.use(bodyParser.json());
-
-server.listen(port, function(){
-    console.log(`Backend is running on ${port}.`);
-})
+const server = require('./config/server');
+require('./config/database');
+require('./config/routes')(server);
